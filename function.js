c0ceb359292517a8313a45e7592e0a44ba05d1ca
flerchai <97056884+flerkendroid@users.noreply.github.com>
2022-08-01 18:49:25 -0400

    function { }
    Habíamos aprendido a crear funciones, una función sin parámetro, que era saltar línea, y ahora vamos a continuar con esos conceptos de funciones, solo que vamos a elevar un poquito el nivel de dificultad. Vamos a eliminar esta variable, que está aquí de sobra porque hay un salto de línea y estamos usando una función. Ahora lo que vamos a hacer es lo siguiente.

---
+++ function.js
@@ -0,0 +1,36 @@
+function saltarlinea()  {
+
+    document.write("<br>");
+    document.write("<br>");
+    document.write("<br>");
+    
+
+    } 
+
+        function imprimir(frase) {
+
+            document.write(frase)
+            saltarlinea();
+
+        }
+
+    document.write("hola panas")
+
+
+    var anho = 2025;
+
+    imprimir("hola panas");
+
+
+    //los programadores acostumbramos a colocar comentarios sobre de qué se trata el código
+    //pues este código calcula las edades de Pedro, Juan y el otro weon xd
+
+    imprimir("Pedro tiene: " +(anho-2000) + " años");
+
+    alert("Joder, esto sí es programación ");
+
+    imprimir("Juan tiene: " +(anho-1995) + " años");
+
+    anho = 2030
+
+    imprimir("Diego tiene: " +(anho-2005) + " años");
